2cd37bf98db3d47842d1119133966d68388d8001
VellyD <viddvd3@gmail.com>
2023-04-14 19:05:15 +0300

    connecting the jokes with the text-to-speech fn.

--- script.js
+++ script.js
@@ -105,24 +105,26 @@ const VoiceRSS = {
   },
 };
 
-// function talk() {
-//   VoiceRSS.speech({
-//     key: "8fddb9759b1d4d269902bc66bc8a1b65",
-//     src: "Hello, Tim!",
-//     hl: "en-us",
-//     v: "Linda",
-//     r: 0,
-//     c: "mp3",
-//     f: "44khz_16bit_stereo",
-//     ssml: false,
-//   });
-// }
-
-// talk();
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+function tellMeAJoke(joke) {
+  VoiceRSS.speech({
+    key: "8fddb9759b1d4d269902bc66bc8a1b65",
+    src: joke,
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
 
 async function getJokes() {
   let joke = "";
   const jokesAPI = "https://v2.jokeapi.dev/joke/Programming";
+
   try {
     const response = await fetch(jokesAPI);
     const data = await response.json();
@@ -132,10 +134,12 @@ async function getJokes() {
       joke = data.joke;
     }
 
-    console.log(joke);
+    tellMeAJoke(joke);
+    toggleButton();
   } catch (err) {
     alert("Server error" + err);
   }
 }
 
-getJokes();
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
